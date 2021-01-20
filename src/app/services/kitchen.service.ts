24b3d0eae2bdbee5926a4175fb1ad766c24f0d56
CNeefs <r0764900@student.thomasmore.be>
2021-01-20 14:56:10 +0100

    kitchencomponent is done!

--- src/app/services/kitchen.service.ts
+++ src/app/services/kitchen.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import {Kitchen} from '../models/kitchen.model';
 
 @Injectable({
@@ -10,19 +10,10 @@ export class KitchenService {
 
   BaseURL: string = "http://localhost:8080/kitchens"
 
-  private kitchens: Kitchen[] = [];
-  private kitchensUpdated = new Subject<Kitchen[]>();
-
   constructor(private http: HttpClient) { }
 
-  getKitchens(){
-    return this.http.get<Kitchen[]>(this.BaseURL).subscribe((kitchens)=>{
-    this.kitchens = kitchens;
-    this.kitchensUpdated.next([...this.kitchens]);
-    })
+  getKitchens(): Observable<Kitchen[]> {
+    return this.http.get<Kitchen[]>(this.BaseURL);
   }
-
-  getKitchensUpdateListener() {
-    return this.kitchensUpdated.asObservable();
-}
+  
 }
