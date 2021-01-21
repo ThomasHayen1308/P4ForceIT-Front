b83ac12ebdb065cfac284c8d4534dc258beb21ab
CNeefs <r0764900@student.thomasmore.be>
2021-01-21 09:29:20 +0100

    start reservations menu, translated kitchen a bit

---
+++ src/app/reservations/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss', '../../styles/page_style.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  toHome(){
+    this.router.navigate(['/home']);
+  }
+
+  toReserveMeeting(){}
+
+  toReserveDesk(){}
+
+}
