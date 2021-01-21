cc384b29c1a3b3cd2f4df6b600b389a2f9344e0e
racoon0024 <r0740911@student.thomasmore.be>
2021-01-21 11:44:07 +0100

    My reservations table ok

--- src/app/my-reservations/my-reservations.component.ts
+++ src/app/my-reservations/my-reservations.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { Reservation } from '../models/reservation.model';
 
-import {ReservationService} from '../services/reservation.service';
+import { ReservationService } from '../services/reservation.service';
+
+interface showReservationForUser {
+  id: number,
+  periode: String,
+  date: Date,
+  chair: String,
+  campus: String,
+  section: String,
+}
 
 @Component({
   selector: 'app-my-reservations',
@@ -15,15 +24,17 @@ import {ReservationService} from '../services/reservation.service';
   styleUrls: ['./my-reservations.component.scss', '../styles/page_style.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
 export class MyReservationsComponent implements OnInit {
   reservationsForUser: Reservation[] = [];
-  dataSource = new MatTableDataSource<Reservation>(this.reservationsForUser);
+  showReservationsForUser: showReservationForUser[] = [];
+
+  dataSource = new MatTableDataSource<showReservationForUser>(this.showReservationsForUser);
   columnsToDisplay = ['Datum', 'Campus', 'Sectie', 'Stoel'];
   expandedElement: Reservation | null;
 
@@ -34,8 +45,17 @@ export class MyReservationsComponent implements OnInit {
   ngOnInit(): void {
     this.reservationService.getReservationsByUserId(1).subscribe((reservations: Reservation[]) => {
       this.reservationsForUser = reservations;
-      console.log(reservations);
-      this.dataSource = new MatTableDataSource(this.reservationsForUser);
+      reservations.map((reservation) => {
+        this.showReservationsForUser.push({
+          id: reservation.id,
+          periode: reservation.period,
+          date: reservation.date,
+          chair: reservation.chair.name,
+          campus: reservation.chair.section.campus.name,
+          section: reservation.chair.section.name,
+        })
+      })
+      this.dataSource = new MatTableDataSource(this.showReservationsForUser);
     })
   }
 
