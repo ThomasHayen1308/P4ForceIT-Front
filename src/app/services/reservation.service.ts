0f84f8ad6b149003c2c4879efa74f30b14b6b765
CNeefs <r0764900@student.thomasmore.be>
2021-01-22 10:43:11 +0100

    changed colors

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Chair } from '../models/chair.model';
 import { Injectable, OnDestroy } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 
 import { Reservation } from '../models/reservation.model';
 import { Section } from '../models/section.model';
+import { MeetingRoom } from '../models/meeting-room.model';
 
 @Injectable({
     providedIn: 'root'
@@ -29,10 +30,15 @@ export class ReservationService implements OnDestroy {
     getChairsBySectionId(sectionId: number){
         return this.http.get<Chair[]>(this.baseUrl+"reservations/section/"+sectionId+"/chairs");
     }
+
     deleteReservation(reservationId: number) {
         this.deleteReservationSub = this.http.delete(this.baseUrl + 'reservations/' + reservationId).subscribe(() => {
             this.reservationDelete.next(reservationId);
         })
+    }
+
+    getMeetingRooms(): Observable<MeetingRoom[]> {
+        return this.http.get<MeetingRoom[]>(this.baseUrl+"meetings/rooms");
     }
 
     ngOnDestroy() {
